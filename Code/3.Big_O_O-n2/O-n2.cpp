4ad6cdb9e9c10f76bc36c879a807fa693cd4b434
dinhtuan.cao <tuancaodinh99@gmail.com>
2024-01-20 16:23:56 +0700

    Update big o code

---
+++ Code/3.Big_O_O-n2/O-n2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+void printItems(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << i << j << endl;
+        }
+    }
+}
+
+
+int main() { 
+
+    printItems(10);
+
+}
